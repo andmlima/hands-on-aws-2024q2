b8fb67e9874b498cbee1d4d103b1cee4be27b466
andmlima <andersonlima.macedo@gmail.com>
2024-07-10 16:34:30 -0300

    adding eks file

---
+++ infrastructure/eks.tf
@@ -0,0 +1,32 @@
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = var.aws_eks_name
+  cluster_version = var.aws_eks_version
+
+  enable_cluster_creator_admin_permissions = true
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = { }
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+      instance_types = var.aws_eks_managed_node_groups_instance_types
+      tags           = var.aws_project_tags
+    }
+  }
+
+  tags = var.aws_project_tags
+}
+
